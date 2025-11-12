9b4e964822f24282a7eff5d266be7b49b8238762
ngoc160304 <145764003+ngoc160304@users.noreply.github.com>
2025-11-12 23:52:13 +0700

    update brevo

--- retaste-be/src/providers/brevo.provider.ts
+++ retaste-be/src/providers/brevo.provider.ts
@@ -1,5 +1,5 @@
 import pug from 'pug';
-import path from 'path';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const brevo = require('@getbrevo/brevo');
 const apiInstance = new brevo.TransactionalEmailsApi();
 import env from '~/configs/environments';
@@ -7,39 +7,39 @@ import env from '~/configs/environments';
 const apiKey = apiInstance.authentications['apiKey'];
 apiKey.apiKey = env.BREVO_API_KEY;
 
-const templatePath = path.resolve(__dirname, '../templates/verify-account.pug');
-
 const verifyAccount = async (email: string, verifyToken: string) => {
   const sendSmtpEmail = new brevo.SendSmtpEmail();
 
   sendSmtpEmail.subject = 'Verify your email';
-  sendSmtpEmail.htmlContent = pug.renderFile(templatePath, { verifyToken });
-
+  sendSmtpEmail.htmlContent = pug.renderFile(
+    'C:\\Users\\ACER\\Workspace\\full-stack-project\\e-commerce\\e-commerce-api\\src\\templates\\verify-account.pug',
+    {
+      verifyToken
+    }
+  );
   sendSmtpEmail.sender = {
     name: env.EMAIL_NAME,
     email: env.EMAIL_ADDRESS
   };
-
-  sendSmtpEmail.to = [{ email }];
-
+  sendSmtpEmail.to = [{ email: email }];
   return apiInstance.sendTransacEmail(sendSmtpEmail);
 };
 const verifyShop = async (email: string) => {
   const sendSmtpEmail = new brevo.SendSmtpEmail();
 
   sendSmtpEmail.subject = 'Verify your email';
-  sendSmtpEmail.htmlContent = pug.renderFile(templatePath);
-
+  sendSmtpEmail.htmlContent = pug.renderFile(
+    'C:\\Users\\ACER\\Workspace\\full-stack-project\\e-commerce\\e-commerce-api\\src\\templates\\verify-account.pug'
+  );
   sendSmtpEmail.sender = {
     name: env.EMAIL_NAME,
     email: env.EMAIL_ADDRESS
   };
-
-  sendSmtpEmail.to = [{ email }];
-
+  sendSmtpEmail.to = [{ email: email }];
   return apiInstance.sendTransacEmail(sendSmtpEmail);
 };
 export const BrevoProvider = {
   verifyAccount,
   verifyShop
 };
+// C:\Users\ACER\Workspace\full-stack-project\e-commerce\e-commerce-api\src\providers
