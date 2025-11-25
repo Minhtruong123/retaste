d282d5f5d0264c867da59826d329da69e197278c
ngoc160304 <145764003+ngoc160304@users.noreply.github.com>
2025-11-25 17:56:47 +0700

    feature upload

--- retaste-be/src/models/repositories/delivery.repo.ts
+++ retaste-be/src/models/repositories/delivery.repo.ts
@@ -11,13 +11,45 @@ const getDeliveryByOrderId = async (orderId: string) => {
 const craeteNew = async (data: IDelivery) => {
   return await Delivery.insertOne(data);
 };
-const getAll = async () => {
-  return await Delivery.find({
-    $or: [{ status: 'pending' }, { status: 'success' }]
-  });
+const getRateDelivery = async () => {
+  return await Delivery.aggregate([
+    {
+      $match: {
+        status: { $in: ['success', 'pending'] },
+        isDeleted: false
+      }
+    },
+    {
+      $group: {
+        _id: null,
+        total: { $sum: 1 },
+        successCount: {
+          $sum: {
+            $cond: [{ $eq: ['$status', 'success'] }, 1, 0]
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        total: '$total',
+        successCount: '$successCount',
+        successRate: {
+          $cond: [
+            { $eq: ['$total', 0] },
+            0,
+            {
+              $multiply: [{ $divide: ['$successCount', '$total'] }, 100]
+            }
+          ]
+        }
+      }
+    }
+  ]);
 };
 export const deliveryRepo = {
   getDeliveryByOrderId,
   craeteNew,
-  getAll
+  getRateDelivery
 };
